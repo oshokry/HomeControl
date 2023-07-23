e990c20bd068577198869c6cc7937b235d7e0503
oshokry <oshokry1@yahoo.com>
2023-07-22 21:23:51 -0500

    Add support for setting the timezone

--- secrets/create.sh
+++ secrets/create.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 kubectl apply -f mysql-secret.yaml
 kubectl apply -f control-config-map.yaml
+kubectl apply -f timezone-config-map.yaml
--- secrets/delete.sh
+++ secrets/delete.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 kubectl delete ConfigMap control-config-map
 kubectl delete secret mysql-secret
+kubectl delete ConfigMap timezone-config-map
